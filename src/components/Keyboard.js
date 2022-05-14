3c674e17eb51fdc48dd674896b89137fbb81606a
nali556 <noman710@protonmail.com>
2022-05-14 18:09:19 -0400

    board ready, start keybaord

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Keyboard() {
   return (
-    <div>Keyboard</div>
+    <div className="keyboard">
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+    </div>
   )
 }
