88ba07d814ab4be90cb2fdc82ff29482b32a6c76
nali556 <noman710@protonmail.com>
2022-05-14 18:50:03 -0400

    Click on keyboard to change first box so far

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
+import { AppContext } from "../App.js";
 
 export default function Key({ keyValue, bigKey }) {
-  return <div className="key" id={bigKey && "big"}>{keyValue}</div>;
+    const { board, setBoard } = useContext(AppContext);
+    const selectLetter = () => {
+        const newBoard = [...board]
+        newBoard[0][0] = keyValue
+        setBoard(newBoard)
+    }
+  return <div className="key" id={bigKey && "big"} onClick={selectLetter}>{keyValue}</div>;
 }
