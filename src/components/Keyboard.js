228ec1943ec83c8c62b6065dd49bc8f4862999dd
nali556 <noman710@protonmail.com>
2022-05-14 18:16:17 -0400

    Added arrays of the lines, mapping them to display

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Keyboard() {
+  //Map through the array in order to display each letter on keyboard
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"]; //Delete and enter separate
+
   return (
     <div className="keyboard">
-      <div className="line1"></div>
+      <div className="line1">
+        {keys1.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
       <div className="line2"></div>
       <div className="line3"></div>
     </div>
-  )
+  );
 }
