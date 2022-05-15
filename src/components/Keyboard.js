0e10371060eeeca5aa287abb96e1d47794bff748
nali556 <noman710@protonmail.com>
2022-05-15 17:17:06 -0400

    adding keyboard functionality

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback, useEffect} from "react";
 import Key from "./Key";
 
 export default function Keyboard() {
@@ -6,6 +6,24 @@ export default function Keyboard() {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"]; //Delete and enter separate
+
+  const handleKeyboard = useCallback((event) =>{
+    if (event.key === "Enter"){
+      
+    }else if(event.key === "Backspace"){
+
+    }else{
+      
+    }
+  })
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard)
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard)
+    }
+  }, [handleKeyboard])
+
 
   return (
     <div className="keyboard">
