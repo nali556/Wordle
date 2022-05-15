ba01fc57e86b2bf209f3ae542cabe884a901669f
nali556 <noman710@protonmail.com>
2022-05-14 22:36:11 -0400

    adjustments to functions so that they are provided everywhere

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,23 +2,14 @@ import React, { useContext } from "react";
 import { AppContext } from "../App.js";
 
 export default function Key({ keyValue, bigKey }) {
-  const { board, setBoard, currAttempt, setAttempt } = useContext(AppContext);
+  const {onSelectLetter, onDelete, onEnter} = useContext(AppContext);
   const selectLetter = () => {
     if (keyValue === "ENTER") {
-      if (currAttempt.letterPos !== 5) return;
-      setAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+      onEnter()
     } else if (keyValue === "DELETE") {
-        if(currAttempt.letterPos === 0) return;
-        const newBoard = [...board];
-        newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = ""
-        setBoard(newBoard)
-        setAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1})
+        onDelete()
     } else {
-      if (currAttempt.letterPos > 4) return;
-      const newBoard = [...board];
-      newBoard[currAttempt.attempt][currAttempt.letterPos] = keyValue;
-      setBoard(newBoard);
-      setAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+        onSelectLetter(keyValue)
     }
   };
   return (
