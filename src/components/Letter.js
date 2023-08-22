739cc7fc074bbcaccedbb1d2605b3a8bf0bf16de
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-08-22 18:40:11 -0400

    word bank added

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,13 +2,12 @@ import React, { useContext } from "react";
 import { AppContext } from "../App.js";
 
 export default function Letter({ letterPos, attemptValue }) {
-  const { board, correctWord } = useContext(AppContext);
+  const { board, correctWord, currAttempt } = useContext(AppContext);
   const letter = board[attemptValue][letterPos];
-
-  const correct = correctWord[letterPos] === letter
-  const almost = !correct && letter !== "" && correctWord.includes(letter) 
-
-  const letterState = correct ? "correct" : almost ? "almost" :"error"
+  console.log(board)
+  const correct = correctWord[letterPos].toUpperCase() === letter.toUpperCase()
+  const almost = !correct && letter !== "" && correctWord.includes(letter.toLowerCase()) 
+  const letterState = currAttempt.attempt > attemptValue && (correct ? "correct" : almost ? "almost" : "error")
 
   return <div className="letter" id={letterState}> {letter}</div>;
 }
